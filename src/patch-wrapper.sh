a103cbc10a42161ff517d9693cea445be68f4d0e
Nikolay Kasyanov <corrmage@gmail.com>
2021-05-01 21:41:10 +0200

    Improve patch wrapper
    - fail immediately if something fails
    - silence preflight check completely

--- src/patch-wrapper.sh
+++ src/patch-wrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 
 PATCHFILE=$1
 
-if ! patch -N -R -p1 --dry-run --silent < "$PATCHFILE"; then
+if ! patch -N -R -p1 --dry-run --silent < "$PATCHFILE" 2>&1 > /dev/null; then
     patch -p1 < "$PATCHFILE"
 else
-    echo "It looks like patch was already applied"
+    echo "It looks like the patch was already applied."
 fi
